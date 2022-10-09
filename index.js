fc28e4c26ffe649517c86f1b92f14d2c1b8fda24
amanthajayathilake <amanthajayathilake@gmail.com>
2022-10-09 12:15:31 +0530

    user management implementation

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import express from "express";
 import cors from "cors";
 import {connect} from './utils/dbConnect.js';
 import chalk from "chalk";
+import ApiRouter from "./routes/index.js";
 
 const portCon = chalk.blue.bgWhite.bold;
 
 const App = express();
 App.use(express.json());
 App.use(cors({origin: '*'}));
+App.use("/api", ApiRouter);
 
 connect();
 
